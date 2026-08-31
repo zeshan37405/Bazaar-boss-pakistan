5d3048dcb03415baede7d927c6a8a8e2b066b7f0
zeshan37405 <zeshan733@gmail.com>
2026-09-01 00:07:29 +0500

    Add Room database

---
+++ confectionery_order_app/app/src/main/java/com/example/confectionery/data/AppDatabase.kt
@@ -0,0 +1,16 @@
+package com.example.confectionery.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [UserEntity::class, CustomerEntity::class, ProductEntity::class, OrderEntity::class, OrderItemEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+    abstract fun customerDao(): CustomerDao
+    abstract fun productDao(): ProductDao
+    abstract fun orderDao(): OrderDao
+}
