4b0614e5fad25c1f736df845ed0802d18b208e89
zeshan37405 <zeshan733@gmail.com>
2026-09-01 05:56:37 +0500

    Set confectionery app V2 version

--- confectionery_order_app/app/build.gradle.kts
+++ confectionery_order_app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.confectionery"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
     }
 
     buildTypes {
